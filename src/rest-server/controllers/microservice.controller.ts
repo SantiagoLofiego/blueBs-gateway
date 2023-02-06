9a6885441e2b5d017f35b3007f582e29de688b16
Juan Marderwald <jmarderwald87@gmail.com>
2023-02-06 15:56:53 -0300

    Cambio en el archivo MicroserviceController, para que sea clase y exportar la clase directamente, y el microservice.router importar la clase directamente

--- src/rest-server/controllers/microservice.controller.ts
+++ src/rest-server/controllers/microservice.controller.ts
@@ -3,44 +3,92 @@ import { MicroServiceService } from "../../domain/services/microService.service"
 
 let service: MicroServiceService = new MicroServiceService();
 
-export const addMicroservice = async (
-  req: Request<{ service: ServiceInstance }>,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    let serviceToAdd = req.params.service;
-    await service.add(serviceToAdd);
-    return res.json(serviceToAdd);
-  } catch (error) {
-    next(error);
-  }
-};
+export class MicroserviceController{
+  constructor(){}
 
-export const removeMicroservice = async (
-  req: Request<{ service: ServiceInstance }>,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    let serviceToAdd = req.params.service;
-    await service.remove(serviceToAdd);
-    return res.json(serviceToAdd);
-  } catch (error) {
-    next(error);
-  }
-};
+  addMicroservice = async (
+    req: Request<{ service: ServiceInstance }>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      let serviceToAdd = req.params.service;
+      await service.add(serviceToAdd);
+      return res.json(serviceToAdd);
+    } catch (error) {
+      next(error);
+    }
+  };
 
-export const getServiceUrl = (
-  req: Request<{ serviceName: string }>,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const serviceName: string = req.params.serviceName;
-    const serviceInstance = service.getService(serviceName);
-    return res.json(serviceInstance);
-  } catch (error) {
-    next(error);
-  }
-};
+  removeMicroservice = async (
+    req: Request<{ service: ServiceInstance }>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      let serviceToAdd = req.params.service;
+      await service.remove(serviceToAdd);
+      return res.json(serviceToAdd);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  getServiceUrl = (
+    req: Request<{ serviceName: string }>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const serviceName: string = req.params.serviceName;
+      const serviceInstance = service.getService(serviceName);
+      return res.json(serviceInstance);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+  
+}
+
+// export const addMicroservice = async (
+//   req: Request<{ service: ServiceInstance }>,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     let serviceToAdd = req.params.service;
+//     await service.add(serviceToAdd);
+//     return res.json(serviceToAdd);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// export const removeMicroservice = async (
+//   req: Request<{ service: ServiceInstance }>,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     let serviceToAdd = req.params.service;
+//     await service.remove(serviceToAdd);
+//     return res.json(serviceToAdd);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// export const getServiceUrl = (
+//   req: Request<{ serviceName: string }>,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     const serviceName: string = req.params.serviceName;
+//     const serviceInstance = service.getService(serviceName);
+//     return res.json(serviceInstance);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- src/rest-server/routes/microService.router.ts
+++ src/rest-server/routes/microService.router.ts
@@ -1,20 +1,19 @@
 import {
-  addMicroservice,
-  removeMicroservice,
-  getServiceUrl,
+  MicroserviceController
 } from "../controllers/microservice.controller";
 
 const express = require("express");
 
 const router = express.Router();
 const microServiceServiceRouter = express.Router();
+const controller = new MicroserviceController();
 
 //Definición de endpoints
-router.get("/:microservice", addMicroservice);
+router.get("/:microservice", controller.addMicroservice);
 
-router.delete("/:microservice", removeMicroservice);
+router.delete("/:microservice", controller.removeMicroservice);
 
-router.get("/:serviceName", getServiceUrl);
+router.get("/:serviceName", controller.getServiceUrl);
 
 //Adición de ruta raíz para '/microservice'
 microServiceServiceRouter.use("/microservice", router);
