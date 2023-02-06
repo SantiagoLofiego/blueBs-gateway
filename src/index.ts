f2a6cab33d63b79f83848e9df9efafc98245c1c2
Juan Carlos Vilcherrez <juanc6v@gmail.com>
2023-02-06 11:40:20 -0300

    server-config

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
-import express, { Request, Response } from "express";
-import config from "./config";
-import microServiceServiceRouter from "./rest-server/routes/microService.router";
-
 import { Server } from "./rest-server/server/server";
 
-const port: string = config.server.port || "3000";
-const server = express();
-const serverMicroService = new Server();
+const main = async () => {
+  const server = new Server(3000);
+  await server.run();
+};
 
-server.use(express.json());
-
-server.all("/api/:value", (req: Request, resp: Response) => {
-  console.log(req.socket.address());
-  resp.send(`Response from api with ${req.params.value} param`);
-});
-
-serverMicroService.run(port);
-serverMicroService.start(microServiceServiceRouter);
-
-//ruter principal en index para todos los rutas
+main();
--- src/rest-server/server/server.ts
+++ src/rest-server/server/server.ts
@@ -1,26 +1,42 @@
-import express, { Router, Express } from "express";
+import express, { Express, Response, Request } from "express";
+import microServiceServiceRouter from "../routes/microService.router";
 
 export class Server {
-  private static instance: Server;
+  private instance?: Server;
   private express: Express;
 
-  constructor() {
+  constructor(private port?: number | string) {
     this.express = express();
+    this.settings();
+    this.middleware();
+    this.start;
   }
 
-  static getInstance(): Server {
-    if (!Server.instance) {
-      Server.instance = new Server();
+  getInstance(): Server {
+    if (!this.instance) {
+      this.instance = new Server();
     }
-    return Server.instance;
+    return this.instance;
   }
 
-  run(port: string): void {
-    this.express.listen(port, () => {
-      console.log(`Server running on port ${port}`);
+  private settings() {
+    this.express.set("port", this.port || process.env.PORT || "3000");
+  }
+
+  private middleware() {
+    this.express.use(express.json());
+  }
+
+  async run(): Promise<void> {
+    await this.express.listen(this.port, () => {
+      console.log(`Server running on port ${this.express.get("port")}`);
     });
   }
-  start(router: Router) {
-    this.express.use(`/server/`, router);
+  private start(): void {
+    this.express.all("api/:value", (req: Request, resp: Response) => {
+      console.log(req.socket.address());
+      resp.send(`Response from api with ${req.params.value} param`);
+    });
+    this.express.use(`/server`, microServiceServiceRouter);
   }
 }
